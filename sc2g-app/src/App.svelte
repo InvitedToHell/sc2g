465c3767aa668b3bf9f1cafe4b961916944d7ddc
InvitedToHell <BrianDETeam@gmail.com>
2023-03-04 00:16:49 +0100

    Created Svelteapp
    https://maurogarcia.dev/maurogarcia.dev/posts/how-to-build-your-next-chrome-extension-with-svelte/

---
+++ sc2g-app/src/App.svelte
@@ -0,0 +1,18 @@
+<head>
+<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+</head>
+
+<script>
+</script>
+
+<body>
+	<main class="container"></main>
+	<button>Button</button>
+	<input type="submit">
+</body>
+
+<style>
+	body {
+		background-color: #F26F23;
+	}
+</style>
